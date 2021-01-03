d1929e05c697ea1270422fef313bc964431feb62
HSHyeon <hongsohyun86@gmail.com>
2021-01-03 10:51:32 +0900

    터치 시 좌표 출력

--- app/src/main/java/com/example/virtualkeypad/MainActivity.kt
+++ app/src/main/java/com/example/virtualkeypad/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.virtualkeypad
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import android.view.Menu
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val view: View = MyView(this)
+        setContentView(view)
     }
-}
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+      //  menuInflater.inflate(R.menu.main, menu) 뭔지 모르겟음
+        return true
+    }
+
+    protected inner class MyView(context: Context?) : View(context) {
+        override fun onTouchEvent(event: MotionEvent): Boolean {
+         
+            super.onTouchEvent(event)
+
+            //event
+            //event 종류/각각의 특성
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                val x = event.x
+                val y = event.y
+                val msg = "터치를 입력받음 : $x / $y"
+                Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
+                return true
+            }
+            return false
+        }
+    }}
