aad1275cd27b93a1b7233554dd9fbb3370f1676d
LeeJE20 <zaq0107@naver.com>
2021-07-25 17:45:25 +0900

    trying navigation-left button event

--- app/src/main/java/com/example/virtualkeypad/Chunjiinactivity.java
+++ app/src/main/java/com/example/virtualkeypad/Chunjiinactivity.java
@@ -43,8 +43,9 @@ public class Chunjiinactivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chunjiin);
 
+//        et = (EditText)findViewById(R.id.chunjiin_text);
         et = (EditText)findViewById(R.id.chunjiin_text);
-        btn = new Button[13];
+        btn = new Button[14];
         btn[0] = (Button)findViewById(R.id.chunjiin_button0);
         btn[1] = (Button)findViewById(R.id.chunjiin_button1);
         btn[2] = (Button)findViewById(R.id.chunjiin_button2);
@@ -58,6 +59,8 @@ public class Chunjiinactivity extends Activity
         btn[10] = (Button)findViewById(R.id.chunjiin_buttonex1);
         btn[11] = (Button)findViewById(R.id.chunjiin_buttonex2);
         btn[12] = (Button)findViewById(R.id.chunjiin_buttonex3);
+
+        btn[13] = (Button)findViewById(R.id.chunjiin_button_left);
 
         chunjiin = new Chunjiin(et, btn);
     }
