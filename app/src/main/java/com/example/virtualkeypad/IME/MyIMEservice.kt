c7cd66111e0334b1afe113117c30b3148e842610
LeeJE20 <zaq0107@naver.com>
2021-08-03 22:35:46 +0900

    천지인.kt 파일도 동작하게 함

--- app/src/main/java/com/example/virtualkeypad/IME/MyIMEservice.kt
+++ app/src/main/java/com/example/virtualkeypad/IME/MyIMEservice.kt
@@ -2,11 +2,44 @@ package com.example.virtualkeypad.IME
 
 import android.inputmethodservice.InputMethodService
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import com.example.virtualkeypad.Chunjiin
+import com.example.virtualkeypad.Chunjiin_kt
 import com.example.virtualkeypad.R
 
 class MyIMEservice: InputMethodService() {
+
+    public var chunjiin: Chunjiin_kt? = null
+    public lateinit var btn: Array<Button?>
+    public var et: EditText? = null
+
     override fun onCreateInputView(): View {
         val myKeyboardView: View = layoutInflater.inflate(R.layout.in_app_keyboard, null)
+
+
+
+        et = myKeyboardView.findViewById<EditText>(R.id.chunjiin_text)
+        btn = arrayOfNulls(14)
+        btn[0] = myKeyboardView.findViewById<View>(R.id.chunjiin_button0) as Button
+        btn[1] = myKeyboardView.findViewById<View>(R.id.chunjiin_button1) as Button
+        btn[2] = myKeyboardView.findViewById<View>(R.id.chunjiin_button2) as Button
+        btn[3] = myKeyboardView.findViewById<View>(R.id.chunjiin_button3) as Button
+        btn[4] = myKeyboardView.findViewById<View>(R.id.chunjiin_button4) as Button
+        btn[5] = myKeyboardView.findViewById<View>(R.id.chunjiin_button5) as Button
+        btn[6] = myKeyboardView.findViewById<View>(R.id.chunjiin_button6) as Button
+        btn[7] = myKeyboardView.findViewById<View>(R.id.chunjiin_button7) as Button
+        btn[8] = myKeyboardView.findViewById<View>(R.id.chunjiin_button8) as Button
+        btn[9] = myKeyboardView.findViewById<View>(R.id.chunjiin_button9) as Button
+        btn[10] = myKeyboardView.findViewById<View>(R.id.chunjiin_buttonex1) as Button
+        btn[11] = myKeyboardView.findViewById<View>(R.id.chunjiin_buttonex2) as Button
+        btn[12] = myKeyboardView.findViewById<View>(R.id.chunjiin_buttonex3) as Button
+
+        btn[13] = myKeyboardView.findViewById<View>(R.id.chunjiin_button_left) as Button
+
+        chunjiin = Chunjiin_kt(et!!, btn)
+
+
         return myKeyboardView
     }
 }
---
+++ app/src/main/java/com/example/virtualkeypad/IME/MyIMEserviceJava.java
@@ -0,0 +1,19 @@
+//package com.example.virtualkeypad.IME;
+//
+//import android.inputmethodservice.InputMethodService;
+//import android.view.View;
+//
+//import android.inputmethodservice.InputMethodService;
+//import com.example.virtualkeypad.R;
+//
+//public class MyIMEserviceJava extends InputMethodService {
+//
+//    public View onCreateInputView() {
+//        View myKeyboardView  = layoutInflater.inflate(R.layout.in_app_keyboard, null);
+//
+//
+//
+//
+//        return myKeyboardView;
+//    }
+//}
